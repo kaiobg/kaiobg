c3b98c4985ac2d44281cfe3b43cda080ed90470e
kaiobg <kaiobg@hotmail.com>
2025-02-09 14:45:42 -0300

    gadiel

--- docs/atletas/testegpt.js
+++ docs/atletas/testegpt.js
@@ -392,9 +392,9 @@ document.addEventListener("DOMContentLoaded", () => {
             
             objetivo_principal: "Criar rotina, emagrecer, fortalecer, saúde física e mental e entrar em forma",
             treinos: [
-                {texto: "Treino funcional A", url: "http://kaioguerrero.com/atletas/gabriel_peagno_a"},
-                {texto: "Treino funcional B", url: "http://kaioguerrero.com/atletas/gabriel_peagno_b"},
-                {texto: "Treino casa rápido", url: "http://kaioguerrero.com/atletas/gabriel_peagno_d"},
+                {texto: "Treino A (praça)", url: "http://kaioguerrero.com/atletas/gabriel_peagno_a"},
+                {texto: "Treino B", url: "http://kaioguerrero.com/atletas/gabriel_peagno_b"},
+                {texto: "Treino casa metabólico", url: "http://kaioguerrero.com/atletas/gabriel_peagno_d"},
                 {texto: "Treino Basquete", url: "http://kaioguerrero.com/atletas/gabriel_peagno_c"},
                 { texto: "Treino Corrida", url: "https://docs.google.com/spreadsheets/d/1iiu6vaaa4803hvowzGfDG_Pd6j_8-LPz7OWbcy2wHfQ/edit?usp=sharing" },
                 
