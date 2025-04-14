3d90b577ef3dd1516f68267549654029d562d08c
kaiobg <kaiobg@hotmail.com>
2025-04-14 11:47:07 -0300

    Ajuste treino Leo

--- docs/atletas/testegpt.js
+++ docs/atletas/testegpt.js
@@ -430,7 +430,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 {texto: "Treino  A", url: "http://kaioguerrero.com/atletas/llneco_a"},
                 {texto: "Treino  B", url: "http://kaioguerrero.com/atletas/llneco_b"},
                 {texto: "Treino  C", url: "http://kaioguerrero.com/atletas/llneco_c"},
-                // {texto: "Treino  D", url: "http://kaioguerrero.com/atletas/llneco_d"},
+                {texto: "Treino  D", url: "http://kaioguerrero.com/atletas/llneco_d"},
             ],
             testes:[
                 {texto: "Histórico de treinos", url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRL91_7QgDkLA_Iw4H6zD-A_qYF_Ln_QLPgXXFpUhi6nwLlzQ5qbp3eF-R18Ve9CHxXZzDDUZ76vS85/pubhtml?gid=2017369885&single=true"}
