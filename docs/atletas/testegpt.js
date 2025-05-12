973d5c0bd53c9d611f83518094e8a5f36b2ac738
kaiobg <kaiobg@hotmail.com>
2025-05-12 16:03:17 -0300

    caio

--- docs/atletas/testegpt.js
+++ docs/atletas/testegpt.js
@@ -264,10 +264,10 @@ document.addEventListener("DOMContentLoaded", () => {
             
             objetivo_principal: "Hipertrofia",
             treinos: [
-                {texto: "Treino  A", url: "http://kaioguerrero.com/atletas/caio_vcs_a"},
-                {texto: "Treino  B", url: "http://kaioguerrero.com/atletas/caio_vcs_b"},
-                {texto: "Treino  C", url: "http://kaioguerrero.com/atletas/caio_vcs_c"},
-                {texto: "Treino  Casa", url: "http://kaioguerrero.com/atletas/caio_vcs_d"},
+                {texto: "Treino  só hoje 12/05", url: "http://kaioguerrero.com/atletas/caio_vcs_a"},
+                // {texto: "Treino  B", url: "http://kaioguerrero.com/atletas/caio_vcs_b"},
+                // {texto: "Treino  C", url: "http://kaioguerrero.com/atletas/caio_vcs_c"},
+                // {texto: "Treino  Casa", url: "http://kaioguerrero.com/atletas/caio_vcs_d"},
             ],
             testes:[
                 {texto: "Histórico de treinos", url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRL91_7QgDkLA_Iw4H6zD-A_qYF_Ln_QLPgXXFpUhi6nwLlzQ5qbp3eF-R18Ve9CHxXZzDDUZ76vS85/pubhtml?gid=600279512&single=true"}
