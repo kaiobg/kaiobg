713416874186bd810df55a1f65f9299e2449bb32
Nathália Pissuti <nathaliapissuti@gmail.com>
2025-08-30 21:39:30 -0300

    chore: Update files

--- src/i18n.js
+++ src/i18n.js
@@ -14,7 +14,7 @@ i18n
       escapeValue: false, // not needed for react as it escapes by default
     },
     backend: {
-      loadPath: '/site-kaio/locales/{{lng}}/{{ns}}.json',
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
   });
 
